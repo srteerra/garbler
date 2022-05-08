f0b34fce1bd58f441abd7c2f4a61e3dba5786ac3
Angel Lopez <srterradev@gmail.com>
2022-05-08 11:16:22 -0600

    typography modified

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,6 +1,7 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { LayoutComponent } from './layout/layout.component';
+import { ExamplesComponent } from './examples/examples.component';
 
 const routes: Routes = [
 	{
@@ -18,6 +19,10 @@ const routes: Routes = [
 					import('./home/home.module').then((m) => m.HomeModule)
 			}
 		]
+	},
+	{
+		path: 'example',
+		component: ExamplesComponent
 	},
 	{
 		path: '**',
