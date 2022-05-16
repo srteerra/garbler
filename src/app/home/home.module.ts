55a691deb76a4b7f04ff662e31d60e05a718a02b
Angel Lopez <srterradev@gmail.com>
2022-05-15 18:58:52 -0600

    GAR-27 task completed

--- src/app/home/home.module.ts
+++ src/app/home/home.module.ts
@@ -3,9 +3,11 @@ import { CommonModule } from '@angular/common';
 
 import { HomeRoutingModule } from './home-routing.module';
 import { HomeViewComponent } from './components/home-view/home-view.component';
+import { MaterialModule } from '../material/material.module';
+import { SharedModule } from '../shared/shared.module';
 
 @NgModule({
 	declarations: [HomeViewComponent],
-	imports: [CommonModule, HomeRoutingModule]
+	imports: [CommonModule, HomeRoutingModule, MaterialModule, SharedModule]
 })
 export class HomeModule {}
--- src/app/material/material.module.ts
+++ src/app/material/material.module.ts
@@ -14,6 +14,7 @@ import { MatMenuModule } from '@angular/material/menu';
 import { MatTreeModule } from '@angular/material/tree';
 import { MatStepperModule } from '@angular/material/stepper';
 import { MatListModule } from '@angular/material/list';
+import { MatChipsModule } from '@angular/material/chips';
 
 @NgModule({
 	declarations: [],
@@ -31,7 +32,8 @@ import { MatListModule } from '@angular/material/list';
 		MatTreeModule,
 		MatFormFieldModule,
 		MatStepperModule,
-		MatListModule
+		MatListModule,
+		MatChipsModule
 	],
 	exports: [
 		MatButtonModule,
@@ -46,7 +48,8 @@ import { MatListModule } from '@angular/material/list';
 		MatTreeModule,
 		MatFormFieldModule,
 		MatStepperModule,
-		MatListModule
+		MatListModule,
+		MatChipsModule
 	]
 })
 export class MaterialModule {}
