a094fdbb8eae2ba339ee41d70ad353403a8b4441
Guapura89 <betocrack1228@gmail.com>
2022-07-17 15:49:48 -0600

    GAR-17 Animations test

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import * as AOS from 'aos';
 
 @Component({
 	selector: 'app-root',
@@ -7,4 +8,9 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
 	title = 'garbler';
+
+	ngOnInit() {
+		AOS.init();
+		window.addEventListener('load', AOS.refresh);
+	}
 }
