e97f197af4514958b4925170998306051db90d45
jonocrod12 <jon_ocampo@hotmail.com>
2022-08-15 15:27:31 -0600

    hola

--- src/app/about/about.module.ts
+++ src/app/about/about.module.ts
@@ -1,18 +1,3 @@
-<<<<<<< HEAD
-import { NgModule } from '@angular/core';
-import { CommonModule } from '@angular/common';
-
-import { AboutRoutingModule } from './about-routing.module';
-import { AboutViewComponent } from './components/about-view/about-view.component';
-import { MaterialModule } from '../material/material.module';
-import { SharedModule } from '../shared/shared.module';
-
-@NgModule({
-	declarations: [AboutViewComponent],
-	imports: [CommonModule, AboutRoutingModule, MaterialModule, SharedModule]
-})
-export class AboutModule {}
-=======
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
