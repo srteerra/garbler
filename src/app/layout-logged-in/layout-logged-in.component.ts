85f2ab2868222aaa1ea60bc1d2d2b57621ee56da
Guapura89 <betocrack1228@gmail.com>
2022-08-21 18:46:58 -0600

    GAR-31 Sidenav responsive fixed

--- src/app/layout-logged-in/layout-logged-in.component.ts
+++ src/app/layout-logged-in/layout-logged-in.component.ts
@@ -1,22 +1,27 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, ViewChild } from '@angular/core';
 import { FirebaseService } from '../services/firebase.service';
 import { Router } from '@angular/router';
+import { MatSidenav } from '@angular/material/sidenav';
 import { MediaMatcher } from '@angular/cdk/layout';
 import { ChangeDetectorRef, OnDestroy } from '@angular/core';
+import { BreakpointObserver } from '@angular/cdk/layout';
 
 @Component({
 	selector: 'app-layout-logged-in',
 	templateUrl: './layout-logged-in.component.html',
 	styleUrls: ['./layout-logged-in.component.scss']
 })
-export class LayoutLoggedInComponent implements OnDestroy {
+export class LayoutLoggedInComponent {
 	mobileQuery: MediaQueryList;
+	@ViewChild(MatSidenav)
+	sidenav!: MatSidenav;
 
 	private _mobileQueryListener: () => void;
 
 	constructor(
 		private firebaseService: FirebaseService,
 		private router: Router,
+		private observer: BreakpointObserver,
 		changeDetectorRef: ChangeDetectorRef,
 		media: MediaMatcher
 	) {
@@ -24,7 +29,6 @@ export class LayoutLoggedInComponent implements OnDestroy {
 		this._mobileQueryListener = () => changeDetectorRef.detectChanges();
 		this.mobileQuery.addListener(this._mobileQueryListener);
 	}
-
 	tlogout() {
 		this.firebaseService
 			.logout()
@@ -34,6 +38,17 @@ export class LayoutLoggedInComponent implements OnDestroy {
 			.catch((err) => console.log(err));
 	}
 
+	ngAfterViewInit() {
+		this.observer.observe(['(max-width: 900px)']).subscribe((res) => {
+			if (res.matches) {
+				this.sidenav.mode = 'over';
+				this.sidenav.close();
+			} else {
+				this.sidenav.mode = 'side';
+				this.sidenav.open();
+			}
+		});
+	}
 	ngOnDestroy(): void {
 		this.mobileQuery.removeListener(this._mobileQueryListener);
 	}
