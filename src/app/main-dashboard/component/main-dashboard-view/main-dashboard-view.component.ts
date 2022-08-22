fdd94d8ac6b85da325bf6b2e69927373f5b15a23
Guapura89 <betocrack1228@gmail.com>
2022-08-21 18:02:44 -0600

    User dashboard view added

--- src/app/main-dashboard/component/main-dashboard-view/main-dashboard-view.component.ts
+++ src/app/main-dashboard/component/main-dashboard-view/main-dashboard-view.component.ts
@@ -7,6 +7,7 @@ import { Component, OnInit } from '@angular/core';
 })
 export class MainDashboardViewComponent {
 	userName = 'Luis Hernandez';
+	userDistrict = 'DISTRICT 5';
 
 	constructor() {}
 }
